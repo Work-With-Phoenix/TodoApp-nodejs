8ca91793bc44781e0a5762a26a776f1525223cf8
tracymuzembi <muzembi33@gmail.com>
2021-07-28 12:38:22 -0400

    post method

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const express = require ('express');
 const app = express();
 
 // rendering static files
-app.use('/static', express.static('public'))
+app.use('/static', express.static('public'));
+app.use(express.urlencoded({extended: true}));
 
 // esj view - engine configuration
 app.set("view engine","ejs");
 
-// adding get response
+// adding get method
 app.get('/', (req, res)=>{
     res.render('todo.ejs')
     
+})
+
+// post method
+app.post('/', (req,res)=>{
+    console.log(req.body);
 })
 app.listen(3000, ()=>console.log("Server up and running"));
