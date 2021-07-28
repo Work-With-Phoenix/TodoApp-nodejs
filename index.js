32d3ac7789e40e392590b928a9abbe7ff33f3506
tracymuzembi <muzembi33@gmail.com>
2021-07-28 11:34:04 -0400

    views templeting setup

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require ('express');
 const app = express();
 
+// rendering static files
+app.use('/static', express.static('public'))
+
+// esj view - engine configuration
+app.set('view engine', 'ejs')
 
 // adding get response
 app.get('/', (req, res)=>{
-    res.send('Hey you!')
+    res.render('todo.ejs')
     
 })
 app.listen(3000, ()=>console.log("Server up and running"));
