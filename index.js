61ff8fa6e12af9b336a394d330558e427a824999
tracymuzembi <muzembi33@gmail.com>
2021-07-28 11:02:06 -0400

    get req,res

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require ('express');
 const app = express();
 
+
+// adding get response
+app.get('/', (req, res)=>{
+    res.send('Hey you!')
+    
+})
 app.listen(3000, ()=>console.log("Server up and running"));
